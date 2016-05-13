c889002f3a68fbcd2269d4ca6301f9837552a01e
Sean Kerr <sean@code-box.org>
2016-05-13 11:54:12 -0700

    renamed src/test/http1/urlencoded.rs -> src/test/http1/url_encoded_field.rs

---
+++ src/test/http1/url_encoded_field.rs
@@ -0,0 +1,147 @@
+// +-----------------------------------------------------------------------------------------------+
+// | Copyright 2016 Sean Kerr                                                                      |
+// |                                                                                               |
+// | Licensed under the Apache License, Version 2.0 (the "License");                               |
+// | you may not use this file except in compliance with the License.                              |
+// | You may obtain a copy of the License at                                                       |
+// |                                                                                               |
+// |  http://www.apache.org/licenses/LICENSE-2.0                                                   |
+// |                                                                                               |
+// | Unless required by applicable law or agreed to in writing, software                           |
+// | distributed under the License is distributed on an "AS IS" BASIS,                             |
+// | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                      |
+// | See the License for the specific language governing permissions and                           |
+// | limitations under the License.                                                                |
+// +-----------------------------------------------------------------------------------------------+
+// | Author: Sean Kerr <sean@code-box.org>                                                         |
+// +-----------------------------------------------------------------------------------------------+
+
+use handler::*;
+use http1::*;
+use test::*;
+use url::*;
+
+macro_rules! setup {
+    ($parser:expr, $handler:expr) => ({
+        $handler.set_content_type(ContentType::UrlEncoded);
+
+        setup(&mut $parser, &mut $handler, b"GET / HTTP/1.1\r\n\r\n", State::UrlEncodedField);
+    });
+}
+
+#[test]
+fn byte_check() {
+    // invalid bytes
+    loop_unsafe(b"\r", |byte| {
+        let mut h = DebugHandler::new();
+        let mut p = Parser::new_request();
+
+        setup!(p, h);
+
+        if let ParserError::UrlEncodedField(x) = assert_error(&mut p, &mut h, &[byte]).unwrap() {
+            assert_eq!(x, byte);
+        } else {
+            panic!();
+        }
+    });
+
+    // valid bytes
+    loop_safe(b"=%", |byte| {
+        let mut h = DebugHandler::new();
+        let mut p = Parser::new_request();
+
+        setup!(p, h);
+
+        assert_eof(&mut p, &mut h, &[byte], State::UrlEncodedField, 1);
+    });
+}
+
+#[test]
+fn callback_exit() {
+    struct X;
+
+    impl HttpHandler for X {
+        fn get_content_type(&mut self) -> ContentType {
+            ContentType::UrlEncoded
+        }
+    }
+
+    impl ParamHandler for X {
+        fn on_param_field(&mut self, _field: &[u8]) -> bool {
+            false
+        }
+    }
+
+    let mut h = X{};
+    let mut p = Parser::new_request();
+
+    setup(&mut p, &mut h, b"GET / HTTP/1.1\r\n\r\n", State::UrlEncodedField);
+
+    assert_callback(&mut p, &mut h, b"Field", State::UrlEncodedField, 5);
+}
+
+#[test]
+fn field() {
+    let mut h = DebugHandler::new();
+    let mut p = Parser::new_request();
+
+    setup!(p, h);
+
+    assert_eof(&mut p, &mut h, b"Field", State::UrlEncodedField, 5);
+    assert_eq!(h.param_field, b"Field");
+}
+
+#[test]
+fn field_ending_ampersand() {
+    let mut h = DebugHandler::new();
+    let mut p = Parser::new_request();
+
+    setup!(p, h);
+
+    assert_eof(&mut p, &mut h, b"Field&", State::UrlEncodedField, 6);
+    assert_eq!(h.param_field, b"Field");
+}
+
+#[test]
+fn field_ending_equal() {
+    let mut h = DebugHandler::new();
+    let mut p = Parser::new_request();
+
+    setup!(p, h);
+
+    assert_eof(&mut p, &mut h, b"Field=", State::UrlEncodedValue, 6);
+    assert_eq!(h.param_field, b"Field");
+}
+
+#[test]
+fn field_ending_percent() {
+    let mut h = DebugHandler::new();
+    let mut p = Parser::new_request();
+
+    setup!(p, h);
+
+    assert_eof(&mut p, &mut h, b"Field%", State::UrlEncodedFieldHex, 6);
+    assert_eq!(h.param_field, b"Field");
+}
+
+#[test]
+fn field_ending_plus() {
+    let mut h = DebugHandler::new();
+    let mut p = Parser::new_request();
+
+    setup!(p, h);
+
+    assert_eof(&mut p, &mut h, b"Field+", State::UrlEncodedField, 6);
+    assert_eq!(h.param_field, b"Field ");
+}
+
+#[test]
+fn field_hex() {
+    let mut h = DebugHandler::new();
+    let mut p = Parser::new_request();
+
+    setup!(p, h);
+
+    assert_eof(&mut p, &mut h, b"Field%21", State::UrlEncodedField, 8);
+    assert_eq!(h.param_field, b"Field!");
+}
