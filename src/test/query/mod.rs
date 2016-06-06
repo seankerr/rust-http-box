3f528eac6e8ab9e064eec871b0bf44c671131f61
Sean Kerr <sean@code-box.org>
2016-06-06 14:54:11 -0700

    moved src/test/url -> src/test/query

---
+++ src/test/query/mod.rs
@@ -0,0 +1,60 @@
+// +-----------------------------------------------------------------------------------------------+
+// | Copyright 2016 Sean Kerr                                                                      |
+// |                                                                                               |
+// | Licensed under the Apache License, Version 2.0 (the "License");                               |
+// | you may not use this file except in compliance with the License.                              |
+// | You may obtain a copy of the License at                                                       |
+// |                                                                                               |
+// |  http://www.apache.org/licenses/LICENSE-2.0                                                   |
+// |                                                                                               |
+// | Unless required by applicable law or agreed to in writing, software                           |
+// | distributed under the License is distributed on an "AS IS" BASIS,                             |
+// | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                      |
+// | See the License for the specific language governing permissions and                           |
+// | limitations under the License.                                                                |
+// +-----------------------------------------------------------------------------------------------+
+// | Author: Sean Kerr <sean@code-box.org>                                                         |
+// +-----------------------------------------------------------------------------------------------+
+
+#[macro_export]
+macro_rules! query {
+    ($stream:expr, $field:expr, $value:expr, $has_field:expr, $has_value:expr,
+     $has_flushed:expr, $length:expr) => ({
+        let mut field       = vec![];
+        let mut has_field   = false;
+        let mut has_flushed = false;
+        let mut has_value   = false;
+        let mut value       = vec![];
+
+        assert!(match parse_query($stream, b'&',
+                                  |segment| {
+                                      match segment {
+                                          QuerySegment::Field(x) => {
+                                              has_field = true;
+                                              field.extend_from_slice(x)
+                                          },
+                                          QuerySegment::Flush => {
+                                              has_flushed = true
+                                          },
+                                          QuerySegment::Value(x) => {
+                                              has_value = true;
+                                              value.extend_from_slice(x)
+                                          }
+                                      }
+                                  }) {
+            Ok($length) => {
+                assert_eq!(field, $field);
+                assert_eq!(value, $value);
+                assert_eq!(has_field, $has_field);
+                assert_eq!(has_value, $has_value);
+                assert_eq!(has_flushed, $has_flushed);
+                true
+            },
+            _ => false
+        });
+    });
+}
+
+mod decode;
+mod parse_query_field;
+mod parse_query_value;
---
+++ src/test/query/parse_query_field.rs
@@ -0,0 +1,116 @@
+// +-----------------------------------------------------------------------------------------------+
+// | Copyright 2016 Sean Kerr                                                                      |
+// |                                                                                               |
+// | Licensed under the Apache License, Version 2.0 (the "License");                               |
+// | you may not use this file except in compliance with the License.                              |
+// | You may obtain a copy of the License at                                                       |
+// |                                                                                               |
+// |  http://www.apache.org/licenses/LICENSE-2.0                                                   |
+// |                                                                                               |
+// | Unless required by applicable law or agreed to in writing, software                           |
+// | distributed under the License is distributed on an "AS IS" BASIS,                             |
+// | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                      |
+// | See the License for the specific language governing permissions and                           |
+// | limitations under the License.                                                                |
+// +-----------------------------------------------------------------------------------------------+
+// | Author: Sean Kerr <sean@code-box.org>                                                         |
+// +-----------------------------------------------------------------------------------------------+
+
+use query::*;
+
+macro_rules! query_error {
+    ($stream:expr, $byte:expr) => ({
+        assert!(match parse_query($stream, b'&',
+                                  |segment| {
+                                      match segment {
+                                          _ => {}
+                                      }
+                                  }) {
+            Err(QueryError::Field(x)) => {
+                assert_eq!(x, $byte);
+                true
+            },
+            _ => false
+        });
+    });
+}
+
+#[test]
+fn basic() {
+    query!(b"Field", b"Field", b"", true, false, true, 5);
+}
+
+#[test]
+fn complex() {
+    query!(b"Field%20Name+%20+%21", b"Field Name   !", b"", true, false, true, 20);
+}
+
+#[test]
+fn ending_ampersand() {
+    query!(b"Field&", b"Field", b"", true, false, true, 6);
+}
+
+#[test]
+fn ending_equal() {
+    query!(b"Field=", b"Field", b"", true, false, true, 6);
+}
+
+#[test]
+fn ending_hex() {
+    query!(b"Field%21", b"Field!", b"", true, false, true, 8);
+}
+
+#[test]
+fn ending_hex_error1() {
+    query_error!(b"Field%", b'%');
+}
+
+#[test]
+fn ending_hex_error2() {
+    query_error!(b"Field%2", b'%');
+}
+
+#[test]
+fn ending_hex_error3() {
+    query_error!(b"Field%2G", b'%');
+}
+
+#[test]
+fn ending_plus() {
+    query!(b"Field+", b"Field ", b"", true, false, true, 6);
+}
+
+#[test]
+fn starting_ampersand_error() {
+    query_error!(b"&", b'&');
+}
+
+#[test]
+fn starting_equal_error() {
+    query_error!(b"=", b'=');
+}
+
+#[test]
+fn starting_hex() {
+    query!(b"%21Field", b"!Field", b"", true, false, true, 8);
+}
+
+#[test]
+fn starting_hex_error1() {
+    query_error!(b"%", b'%');
+}
+
+#[test]
+fn starting_hex_error2() {
+    query_error!(b"%2", b'%');
+}
+
+#[test]
+fn starting_hex_error3() {
+    query_error!(b"%2G", b'%');
+}
+
+#[test]
+fn starting_plus() {
+    query!(b"+Field", b" Field", b"", true, false, true, 6);
+}
