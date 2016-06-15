b658f3dfdd2158685266d946039c0942aa9d4fe4
Sean Kerr <sean@code-box.org>
2016-06-15 15:24:49 -0700

    +src/handler/http1/*

---
+++ src/handler/http1/url_encoded.rs
@@ -0,0 +1,31 @@
+// +-----------------------------------------------------------------------------------------------+
+// | Copyright 2016 Sean Kerr                                                                      |
+// |                                                                                               |
+// | Licensed under the Apache License, Version 2.0 (the "License");                               |
+// | you may not use this file except in compliance with the License.                              |
+// | You may obtain a copy of the License at                                                       |
+// |                                                                                               |
+// |  http://www.apache.org/licenses/LICENSE-2.0                                                   |
+// |                                                                                               |
+// | Unless required by applicable law or agreed to in writing, software                           |
+// | distributed under the License is distributed on an "AS IS" BASIS,                             |
+// | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                      |
+// | See the License for the specific language governing permissions and                           |
+// | limitations under the License.                                                                |
+// +-----------------------------------------------------------------------------------------------+
+// | Author: Sean Kerr <sean@code-box.org>                                                         |
+// +-----------------------------------------------------------------------------------------------+
+
+//! [`Http1Handler`](../../http1/trait.Http1Handler.html) implementation for processing URL encoded
+//! data.
+
+use http1::Http1Handler;
+
+pub struct UrlEncodedHttp1Handler {
+}
+
+impl UrlEncodedHttp1Handler {
+}
+
+impl Http1Handler for UrlEncodedHttp1Handler {
+}
