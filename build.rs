52f76a4989ff975d8bc578ff6d2c96798cb8e6ab
Sean Kerr <sean@code-box.org>
2016-11-01 07:37:56 -0700

    updated build.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 extern crate skeptic;
 
 fn main() {
-    skeptic::generate_doc_tests(&["examples/detect_request_response.md"]);
-    skeptic::generate_doc_tests(&["examples/headers.md"]);
-    skeptic::generate_doc_tests(&["examples/query.md"]);
+    skeptic::generate_doc_tests(&["examples/detect_request_response.md",
+                                  "examples/headers.md",
+                                  "examples/query.md"]);
 }
