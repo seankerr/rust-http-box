141b361c7dc22203f077a102af44fc307c8e61fb
Sean Kerr <sean@code-box.org>
2017-03-14 19:07:44 -0700

    +examples/http_utilities.md

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ extern crate skeptic;
 fn main() {
     skeptic::generate_doc_tests(&[
         "README.md",
+        "examples/http_utilities.md",
         "examples/http1_head_parsing.md",
         "examples/http1_intro.md"
     ]);
