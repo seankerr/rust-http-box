f9f966b637f0754e05646588c55f9a89b2e4c174
Sean Kerr <sean@code-box.org>
2017-03-28 15:51:16 -0700

    cleaned up chunked tests

---
+++ src/http1/test/chunked/trailers.rs
@@ -0,0 +1,48 @@
+// +-----------------------------------------------------------------------------------------------+
+// | Copyright 2016 Sean Kerr                                                                      |
+// |                                                                                               |
+// | Licensed under the Apache License, Version 2.0 (the "License");                               |
+// | you may not use this file except in compliance with the License.                              |
+// | You may obtain a copy of the License at                                                       |
+// |                                                                                               |
+// |  http://www.apache.org/licenses/LICENSE-2.0                                                   |
+// |                                                                                               |
+// | Unless required by applicable law or agreed to in writing, software                           |
+// | distributed under the License is distributed on an "AS IS" BASIS,                             |
+// | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                      |
+// | See the License for the specific language governing permissions and                           |
+// | limitations under the License.                                                                |
+// +-----------------------------------------------------------------------------------------------+
+
+use http1::*;
+use http1::test::*;
+use test::*;
+
+#[test]
+fn trailers() {
+    let (mut p, mut h) = http1_setup!();
+
+    p.init_chunked();
+
+    assert_finished(
+        &mut p,
+        &mut h,
+        b"0\r\n\
+          Trailer1: Value1\r\n\
+          Trailer2: Value2\r\n\r\n",
+        ParserState::Finished,
+        b"0\r\n\
+          Trailer1: Value1\r\n\
+          Trailer2: Value2\r\n\r\n".len()
+    );
+
+    assert_eq!(
+        &h.header_name,
+        b"trailer1trailer2"
+    );
+
+    assert_eq!(
+        &h.header_value,
+        b"Value1Value2"
+    );
+}
